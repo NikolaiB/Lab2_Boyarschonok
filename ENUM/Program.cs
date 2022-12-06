830445f3bfc1292f9e43c3f3fa39b9a3d67934ec
NikolaiB <altnikolos95@gmail.com>
2022-12-06 20:39:03 +0300

    Lab2

---
+++ ENUM/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ENUM
+{
+    internal class Program
+    {
+        enum DaysWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wenesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+
+
+        }
+        static void Main(string[] args)
+        { 
+            
+        string stringnumday = Console.ReadLine();
+            DaysWeek numday = (DaysWeek)Enum.Parse(typeof(DaysWeek), stringnumday);
+            //int numday = Convert.ToInt32(stringnumday);
+            switch (numday)
+            {
+                case DaysWeek.Monday:
+                    break;
+                case DaysWeek.Tuesday:
+                    break;
+                case DaysWeek.Wenesday:
+                    break;
+                case DaysWeek.Thursday:
+                    break;
+                case DaysWeek.Friday:
+                    break;
+                case DaysWeek.Saturday:
+                    break;
+                case DaysWeek.Sunday:
+                    break;
+                default:
+                    Console.WriteLine("Неверное число");
+                    break;
+            }
+            Console.WriteLine(numday);
+
+
+            
+
+
+        }
+
+             
+
+        
+
+        //static void DaysWeek GetNameDay(int numday)
+        // {
+        //   if (numday.)
+
+
+        //  }
+    
+    }
+    
+
+}
---
+++ Lab2_Boyarschonok/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+namespace Lab2_Boyarschonok
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[,] nums = new int[10, 10] 
+            { 
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78,  },
+                { 0, 1, 2, 10, 20, 30, 40, 50, 60, 78, },
+            };
+
+            
+
+          
+
+            //static void GetArifm(int[,] nums)
+            //{
+            double arifm = 0;
+                
+                Console.WriteLine("Output array");
+
+                for (int i = 0; i < nums.GetLength(0); i++)
+                {
+
+                    for (int k = 0; k < nums.GetLength(1); k++)
+                    {
+                        Console.Write(nums[i, k] + "\t");
+                        arifm += nums[i, k];
+
+                    }
+                    Console.WriteLine();
+                }
+
+
+                Console.WriteLine("Арифмитическое = " + "\t" + arifm / nums.Length);
+
+           // }
+
+          
+
+         
+
+
+
+        }
+    }
+}
